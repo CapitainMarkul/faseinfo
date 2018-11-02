78b2b439223b33fac719863430c38f4772377789
Capitain Marcul <dart525@mail.ru>
2018-11-02 09:20:04 +0300

    Initial commit

---
+++ app/src/main/java/sbis/faceinfo/presentation/detailinfo/DetailInfoActivity.kt
@@ -0,0 +1,13 @@
+package sbis.faceinfo.presentation.detailinfo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import sbis.faceinfo.R
+
+class DetailInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_info)
+    }
+}
---
+++ app/src/main/java/sbis/faceinfo/presentation/welcome/SplashActivity.kt
@@ -0,0 +1,13 @@
+package sbis.faceinfo.presentation.welcome
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import sbis.faceinfo.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
