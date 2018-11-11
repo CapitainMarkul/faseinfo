da00b14a0605beaa1bd53a185823e66e6b9b8f18
Capitain Marcul <dart525@mail.ru>
2018-11-11 17:36:59 +0300

    Added: add new behavior for circle user photo

--- app/src/main/java/sbis/faceinfo/presentation/detailinfo/DetailInfoActivity.kt
+++ app/src/main/java/sbis/faceinfo/presentation/detailinfo/DetailInfoActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import sbis.faceinfo.R
 import android.support.v7.app.AppCompatActivity
@@ -16,12 +15,19 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.support.design.widget.AppBarLayout
+import android.view.View
+import android.view.animation.AlphaAnimation
 
 
-
-class DetailInfoActivity : AppCompatActivity() {
+class DetailInfoActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
 
     companion object {
+        private const val PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.8f
+        private const val PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.1f
+        private const val ALPHA_ANIMATIONS_DURATION = 200
+
+
         private val ARG_USER_ID = "ARG_USER_ID"
         private val ARG_USER_TEMP = "ARG_USER_TEMP"
 
@@ -37,11 +43,18 @@ class DetailInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailInfoBinding
 
+    private var isTitleVisible = false
+    private var isTitleContainerVisible = true
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this@DetailInfoActivity, R.layout.activity_detail_info)
+
+
+        binding.appBar.addOnOffsetChangedListener(this)
+        startAlphaAnimation(binding.txtUserName, 0, View.INVISIBLE)
 
 
         binding.personStatistics.setSectionIcons(
@@ -56,7 +69,9 @@ class DetailInfoActivity : AppCompatActivity() {
 
         val user = intent.extras.getParcelable<PersonSearch>(ARG_USER_TEMP)
 
+        //todo: перевести в dataBinding
         binding.txtUserName.text = "${user.secondName} ${user.name}"
+        binding.txtUserNameTitle.text = "${user.secondName} ${user.name}"
         binding.imgUserProfile.setImageDrawable(loadImgFromAsserts(user.photoUrl))
 
     }
@@ -84,5 +99,43 @@ class DetailInfoActivity : AppCompatActivity() {
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
         drawable.draw(canvas)
         return bitmap
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+        val maxScroll = appBarLayout.totalScrollRange
+        val percentage = Math.abs(verticalOffset).toFloat() / maxScroll.toFloat()
+
+        handleAlphaOnTitle(percentage)
+        handleToolbarTitleVisibility(percentage)
+    }
+
+    private fun handleToolbarTitleVisibility(percentage: Float) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR && !isTitleVisible) {
+            startAlphaAnimation(binding.txtUserName, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
+            isTitleVisible = true
+        } else if (percentage < PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR && isTitleVisible) {
+            startAlphaAnimation(binding.txtUserName, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
+            isTitleVisible = false
+        }
+    }
+
+    private fun handleAlphaOnTitle(percentage: Float) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS && isTitleContainerVisible) {
+            startAlphaAnimation(binding.titleCustom, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
+            isTitleContainerVisible = false
+        } else if (percentage < PERCENTAGE_TO_HIDE_TITLE_DETAILS && !isTitleContainerVisible) {
+            startAlphaAnimation(binding.titleCustom, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
+            isTitleContainerVisible = true
+        }
+    }
+
+    private fun startAlphaAnimation(v: View, duration: Long, visibility: Int) {
+        val alphaAnimation =
+            if (visibility == View.VISIBLE) AlphaAnimation(0f, 1f)
+            else AlphaAnimation(1f, 0f)
+
+        alphaAnimation.duration = duration
+        alphaAnimation.fillAfter = true
+        v.startAnimation(alphaAnimation)
     }
 }
