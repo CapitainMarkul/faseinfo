0b2928cea56999b7f150c66fe379eb1ddba37e9e
da.pavlov1 <da.pavlov1@tensor.ru>
2018-11-14 18:34:02 +0300

    Create: create 'Splash Screen'

--- app/src/main/java/sbis/faceinfo/presentation/welcome/SplashActivity.kt
+++ app/src/main/java/sbis/faceinfo/presentation/welcome/SplashActivity.kt
@@ -1,13 +1,18 @@
 package sbis.faceinfo.presentation.welcome
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import sbis.faceinfo.R
+import sbis.faceinfo.presentation.search.view.activity.SearchActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, SearchActivity::class.java))
+        }, 700)
     }
 }
